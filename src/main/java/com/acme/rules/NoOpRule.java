1df1a06a8128fbe77d0c3cd8bbe41907431f93a3
Gilles Querret <g.querret@riverside-software.fr>
2025-12-17 09:29:31 +0100

    Add Impact and CleanCode annotations

--- src/main/java/com/acme/rules/NoOpRule.java
+++ src/main/java/com/acme/rules/NoOpRule.java
@@ -4,6 +4,8 @@ import org.sonar.api.batch.fs.InputFile;
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
 import org.sonar.plugins.openedge.api.checks.OpenEdgeProparseCheck;
+import org.sonar.plugins.openedge.api.model.CleanCode;
+import org.sonar.plugins.openedge.api.model.Impact;
 import org.sonar.plugins.openedge.api.model.SqaleConstantRemediation;
 import org.prorefactor.core.ABLNodeType;
 import org.prorefactor.core.JPNode;
@@ -11,6 +13,16 @@ import org.prorefactor.treeparser.ParseUnit;
 
 @Rule(priority = Priority.INFO, name = "No-op rule", tags = {"acme"})
 @SqaleConstantRemediation(value = "5min")
+// Quality must be one of: SECURITY, RELIABILITY, MAINTAINABILITY
+// Severity must be one of: INFO, LOW, MEDIUM, HIGH, BLOCKER
+@Impact(quality = "MAINTAINABILITY", severity = "INFO")
+// Attribute must be one of:
+//   FORMATTED, CONVENTIONAL, IDENTIFIABLE
+//   CLEAR, LOGICAL, COMPLETE, EFFICIENT 
+//   FOCUSED, DISTINCT, MODULAR 
+//   LAWFUL, TRUSTWORTHY, RESPECTFUL
+// See https://docs.sonarsource.com/sonarqube-server/2025.5/glossary/#c
+@CleanCode(attribute = "FORMATTED")
 public class NoOpRule extends OpenEdgeProparseCheck {
 
   @Override
